72aa3e1a65c4555fd164038d91bbb2cd0bb77006
David <yoxisem544@gmail.com>
2015-04-18 18:19:57 +0800

    adding side bar

--- WeekTable/WeekTimeTableViewController.swift
+++ WeekTable/WeekTimeTableViewController.swift
@@ -19,6 +19,8 @@ class WeekTimeTableViewController: UIViewController {
     // side bar region
     var sideBarHeight: CGFloat!
     var sideBarWidth: CGFloat!
+    var sideBarCellHeight: CGFloat!
+    var sideBarCellWidth: CGFloat!
     
     // grid view region
     var gridTimeTableHeight: CGFloat!
@@ -29,9 +31,14 @@ class WeekTimeTableViewController: UIViewController {
     var screenWidth: CGFloat!
     var screenHeight: CGFloat!
     
+    // course counts?
+    var courseCount: Int! = 9
+    
     // color region
     var headerBarColor: UIColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.7)
     var headerBarCellColor: UIColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.7)
+    var sideBarColor: UIColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.7)
+    var sideBarCellColor: UIColor = UIColor(red: 1.0, green: 1.0, blue: 0.3, alpha: 0.7)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,13 +53,19 @@ class WeekTimeTableViewController: UIViewController {
         
         // side bar setup
         self.sideBarWidth = 40.0
+        self.sideBarHeight = self.screenHeight - self.headerBarHeight
         
         // setup header cell item
         self.headerBarCellHeight = 30.0
         self.headerBarCellWidth = (self.screenWidth - self.sideBarWidth)/5
         
+        // setup side bar cell item
+        self.sideBarCellHeight = self.sideBarHeight/CGFloat(self.courseCount)
+        self.sideBarCellWidth = self.sideBarWidth
+        
         // adding views to user
         self.view.addSubview(self.HeaderBarView())
+        self.view.addSubview(self.SideBarView())
     }
     
     
@@ -70,10 +83,38 @@ class WeekTimeTableViewController: UIViewController {
             // let label can fit inside the mask
             dayView.layer.masksToBounds = true
             
+            
+            // position of x in every day
             var x = self.sideBarWidth + CGFloat(day)*self.headerBarCellWidth + self.headerBarCellWidth/2
             dayView.center = CGPointMake(x, self.headerBarHeight/2)
             
             view.addSubview(dayView)
+        }
+        
+        return view as UIView
+    }
+    
+    func SideBarView() -> UIView {
+        
+        var view = UIView(frame: CGRectMake(0.0, self.headerBarHeight, self.sideBarWidth, self.sideBarHeight))
+        view.backgroundColor = self.sideBarColor
+        
+        // classes in moring NTU got 0 at the very first class
+        // fuck cannot use class so i use session here
+        for session in 0...(self.courseCount - 1) {
+            var sessionLabel = UILabel(frame: CGRectMake(0.0, 0.0, self.sideBarCellWidth, self.sideBarCellHeight))
+            sessionLabel.text = "\(session)"
+            sessionLabel.backgroundColor = self.sideBarCellColor
+            sessionLabel.textAlignment = NSTextAlignment.Center
+            sessionLabel.layer.cornerRadius = 15
+            sessionLabel.layer.masksToBounds = true
+            
+            
+            var y = CGFloat(session)*self.sideBarCellHeight + self.sideBarCellHeight/2
+            sessionLabel.center = CGPointMake(self.sideBarCellWidth/2, y)
+            println("\(session)")
+            
+            view.addSubview(sessionLabel)
         }
         
         return view as UIView
